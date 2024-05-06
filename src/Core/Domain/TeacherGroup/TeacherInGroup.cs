4c84bd624065bf6d8f7c321c437b1f0f380f0314
NguyenVanVuong <hayasilver123@gmail.com>
2024-05-06 12:19:43 +0700

    feature : crud group teacher

---
+++ src/Core/Domain/TeacherGroup/TeacherInGroup.cs
@@ -0,0 +1,7 @@
+﻿namespace FSH.WebApi.Domain.TeacherGroup;
+public class TeacherInGroup
+{
+    public Guid TeacherId { get; set; }
+    public Guid GroupTeacherId { get; set; }
+    public virtual GroupTeacher GroupTeacher { get; set; }
+}
