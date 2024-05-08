981b5bf7c4411b1a3a74689890d6526592335517
ducNT <cavoixanh2112@gmail.com>
2024-05-08 16:13:38 +0700

    update teacher permission in class

--- src/Host/Controllers/TeacherGroup/PermissionClassesController.cs
+++ src/Host/Controllers/TeacherGroup/PermissionClassesController.cs
@@ -1,6 +1,13 @@
-﻿
+﻿using FSH.WebApi.Application.TeacherGroup.PermissionClasses;
 
 namespace FSH.WebApi.Host.Controllers.TeacherGroup;
 public class PermissionClassesController : VersionedApiController
 {
+    [HttpPost("teacher-permission-in-class")]
+    [MustHavePermission(FSHAction.Update, FSHResource.GroupTeachers)]
+    [OpenApiOperation("Set a teacher's permissions for class", "")]
+    public Task SetTeacherPermissionInClass(BulkUpdateTeacherPermissionInClassRequest request)
+    {
+        return Mediator.Send(request);
+    }
 }
