ac255cfb8af8717cf878109bed01a37690331f2a
NguyenVanVuong <hayasilver123@gmail.com>
2024-05-08 17:01:30 +0700

    feature : group permission in class

--- src/Host/Controllers/TeacherGroup/PermissionClassesController.cs
+++ src/Host/Controllers/TeacherGroup/PermissionClassesController.cs
@@ -1,6 +1,23 @@
-﻿
+﻿using FSH.WebApi.Application.TeacherGroup.GroupTeachers;
+using FSH.WebApi.Application.TeacherGroup.PermissionClasses;
 
 namespace FSH.WebApi.Host.Controllers.TeacherGroup;
 public class PermissionClassesController : VersionedApiController
 {
+
+    [HttpGet("{id:guid}")]
+    [MustHavePermission(FSHAction.View, FSHResource.GroupTeachers)]
+    [OpenApiOperation("Get groupTeacher details with permission class", "")]
+    public Task<GroupTeacherDto> GetAsync(Guid id)
+    {
+        return Mediator.Send(new GetGroupTeacherWithPermissionRequest(id));
+    }
+
+    [HttpPost("group-permission-in-class")]
+    [MustHavePermission(FSHAction.Update, FSHResource.GroupTeachers)]
+    [OpenApiOperation("Set a teacher's permissions for class", "")]
+    public Task SetTeacherPermissionInClass(BulkUpdateGroupPermissionInClassRequest request)
+    {
+        return Mediator.Send(request);
+    }
 }
