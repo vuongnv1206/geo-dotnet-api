1544d571acc5ab1744391c0fc17827cffe778bf5
nguyenvancaokyfpt <kynvche163260@fpt.edu.vn>
2024-06-13 21:55:31 +0700

    refactor: Update question and passage DTOs to use consistent naming for parent ID

--- src/Core/Application/Questions/Dtos/QuestionDto.cs
+++ src/Core/Application/Questions/Dtos/QuestionDto.cs
@@ -1,4 +1,5 @@
-﻿using FSH.WebApi.Domain.Question.Enums;
+﻿using FSH.WebApi.Application.Identity.Users;
+using FSH.WebApi.Domain.Question.Enums;
 
 namespace FSH.WebApi.Application.Questions.Dtos;
 
@@ -19,4 +20,5 @@ public class QuestionDto : AuditableEntity, IDto
     public QuestionLableDto? QuestionLable { get; set; }
     public List<QuestionPassagesDto>? QuestionPassages { get; set; } = new();
     public List<AnswerDto>? Answers { get; set; } = new();
+    public UserDetailsDto? Owner { get; set; }
 }
