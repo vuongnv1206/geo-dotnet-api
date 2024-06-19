07e169688708a0f49c537be68385ca77f7773ab3
Nguyễn Văn Cao Kỳ <kynvche163260@fpt.edu.vn>
2024-06-19 15:18:47 +0000

    CRUD Notifications

---
+++ src/Core/Application/Notifications/NotificationDto.cs
@@ -0,0 +1,14 @@
+﻿using static FSH.WebApi.Shared.Notifications.BasicNotification;
+
+namespace FSH.WebApi.Application.Notifications;
+public class NotificationDto : IDto
+{
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Title { get; set; }
+    public LabelType LableType { get; set; }
+    public string Message { get; set; }
+    public string? Url { get; set; }
+    public bool IsRead { get; set; }
+    public DateTime CreatedOn { get; set; }
+}
---
+++ src/Core/Application/Notifications/TestNotificationRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace FSH.WebApi.Application.Notifications;
+public class TestNotificationRequest : IRequest<string>
+{
+    public string[] userIds { get; set; }
+    public string? Message { get; set; }
+}
+
+public class TestNotificationHandler : IRequestHandler<TestNotificationRequest, string>
+{
+    private readonly INotificationSender _notifications;
+
+    public TestNotificationHandler(INotificationSender notifications)
+    {
+        _notifications = notifications;
+    }
+
+    public async Task<string> Handle(TestNotificationRequest request, CancellationToken cancellationToken)
+    {
+        var notification = new BasicNotification
+        {
+            Message = request.Message,
+            Label = BasicNotification.LabelType.Information
+        };
+
+        await _notifications.SendToUsersAsync(notification, request.userIds, cancellationToken);
+
+        return "Notification sent";
+    }
+}
