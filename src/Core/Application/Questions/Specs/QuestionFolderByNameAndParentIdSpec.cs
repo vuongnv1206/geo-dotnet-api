fc233c96e7413e4af641346a2219871bd81bcaac
nguyenvancaokyfpt <kynvche163260@fpt.edu.vn>
2024-06-24 03:47:52 +0700

    feat: Add validation to check for unique folder names

---
+++ src/Core/Application/Questions/Specs/QuestionFolderByNameAndParentIdSpec.cs
@@ -0,0 +1,9 @@
+﻿using FSH.WebApi.Domain.Question;
+
+namespace FSH.WebApi.Application.Questions.Specs;
+public class QuestionFolderByNameAndParentIdSpec : Specification<QuestionFolder>, ISingleResultSpecification
+{
+    public QuestionFolderByNameAndParentIdSpec(string name, Guid? parentId) =>
+        Query
+        .Where(b => b.Name == name && b.ParentId == parentId);
+}
