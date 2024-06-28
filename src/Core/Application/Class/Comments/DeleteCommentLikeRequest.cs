f70c758467bd7e820b12193334f9eeac1fd08df1
MinhTam <Tamnoi2608@gmail>
2024-06-29 01:28:34 +0700

    Edit_classDeteleLikeInComment

--- src/Core/Application/Class/Comments/DeleteCommentLikeRequest.cs
+++ src/Core/Application/Class/Comments/DeleteCommentLikeRequest.cs
@@ -1,4 +1,5 @@
-﻿using FSH.WebApi.Application.Common.Interfaces;
+﻿using FSH.WebApi.Application.Class.Comments.Spec;
+using FSH.WebApi.Application.Common.Interfaces;
 using FSH.WebApi.Application.Identity.Users;
 using FSH.WebApi.Domain.Class;
 using System;
@@ -12,40 +13,34 @@ public class DeleteCommentLikeRequest : IRequest<Guid>
 {
     public Guid CommentId { get; set; }
     public Guid UserId { get; set; }
-    public DeleteCommentLikeRequest(Guid commentId, DefaultIdType userId)
-    {
-        CommentId = commentId;
-        UserId = userId;
-    }
 }
+
 public class DeleteCommentLikeRequestHandler : IRequestHandler<DeleteCommentLikeRequest, Guid>
 {
     private readonly IStringLocalizer _t;
     private readonly IRepository<Comment> _commentRepository;
-    public DeleteCommentLikeRequestHandler(IRepository<Comment> commentRepository
-                                           IStringLocalizer<DeleteCommentLikeRequestHandler> stringLocalizer) =>
+    public DeleteCommentLikeRequestHandler(IRepository<Comment> commentRepository, IStringLocalizer<DeleteCommentLikeRequestHandler> stringLocalizer) =>
+
         (_commentRepository, _t) = (commentRepository, stringLocalizer);
 
     public async Task<Guid> Handle(DeleteCommentLikeRequest request, CancellationToken cancellationToken)
     {
-        var comment = await _commentRepository.GetByIdAsync(request.CommentId);
+        var comment = await _commentRepository.FirstOrDefaultAsync(new CommentByIdSpec(request.CommentId));
         if (comment == null)
         {
             throw new NotFoundException(_t["Comment {0} Not Found.", request.CommentId]);
         }
 
-        if (comment.CommentLikes.Any())
+        var commentlike = comment.CommentLikes.FirstOrDefault(x => x.UserId == request.UserId);
+        if (commentlike == null)
         {
-            var commentlike = comment.CommentLikes?
-                .FirstOrDefault(x => x.UserId == request.UserId);
-
-            if (commentlike is null)
-                throw new NotFoundException(_t["Student {0} Not Found.", request.UserId]);
-
-            comment.RemoveCommentLike(commentlike);
-
-            await _commentRepository.UpdateAsync(comment);
+            throw new NotFoundException(_t["Student {0} Not Found.", request.UserId]);
         }
-        return default(Guid);
+
+        comment.RemoveCommentLike(commentlike);
+
+        await _commentRepository.UpdateAsync(comment);
+
+        return default(DefaultIdType);
     }
 }
