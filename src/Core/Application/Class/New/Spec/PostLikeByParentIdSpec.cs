f2fbb25dd2d8c62084afcbff10160fd1457ca20a
thang1834 <90250933+thang1834@users.noreply.github.com>
2024-06-30 12:53:54 +0700

    Add PostLikeByParentIdSpec for Post queries
    - Introduced new `using` directives for necessary namespaces including `FSH.WebApi.Domain.Class`, `System`, and others.
    - Defined a new namespace `FSH.WebApi.Application.Class.New.Spec` for application layer specifications.
    - Added `PostLikeByParentIdSpec` class, inheriting `Specification<Post>` and implementing `ISingleResultSpecification`.
    - The constructor of `PostLikeByParentIdSpec` uses `postId` to filter `Post` entities by ID, aiming for a single result query.

---
+++ src/Core/Application/Class/New/Spec/PostLikeByParentIdSpec.cs
@@ -0,0 +1,15 @@
+﻿using FSH.WebApi.Domain.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSH.WebApi.Application.Class.New.Spec;
+public class PostLikeByParentIdSpec : Specification<Post>, ISingleResultSpecification
+{
+    public PostLikeByParentIdSpec(DefaultIdType postId)
+    {
+        Query.Where(p => p.Id == postId);
+    }
+}
