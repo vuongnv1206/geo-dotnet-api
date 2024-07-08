8d7b535b00ff497006347e741799e850fa7c7b3b
NguyenVanVuong <hayasilver123@gmail.com>
2024-07-08 23:53:02 +0700

    update question clone

---
+++ src/Core/Application/Examination/PaperFolders/Specs/MyPaperFolderTreeSpec.cs
@@ -0,0 +1,18 @@
+﻿
+
+using FSH.WebApi.Domain.Examination;
+
+namespace FSH.WebApi.Application.Examination.PaperFolders;
+public class MyPaperFolderTreeSpec : Specification<PaperFolder>
+{
+    public MyPaperFolderTreeSpec(Guid currentUserId)
+    {
+        Query
+        .Include(x => x.PaperFolderParent)
+        .Include(x => x.PaperFolderChildrens)
+        .Include(x => x.PaperFolderPermissions)
+        .ThenInclude(x => x.GroupTeacher)
+        .Where(x => x.CreatedBy == currentUserId)
+        .OrderBy(x => x.CreatedOn);
+    }
+}
