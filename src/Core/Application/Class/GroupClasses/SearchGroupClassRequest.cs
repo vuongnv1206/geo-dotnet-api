c3219706edc607557cdcb2179d76714858914e4d
thang1834 <90250933+thang1834@users.noreply.github.com>
2024-07-16 15:30:13 +0700

    Refactor spec implementation for GroupClass
    Refactored the implementation of `GroupClassBySearchRequestSpec` by moving it to its own file `GroupClassBySearchRequestSpec.cs`, improving code organization and maintainability. Adjusted namespaces and `using` directives to align with the new file structure. Enhanced the specification pattern by redefining `GroupClassBySearchRequestSpec` as an internal class extending `Specification<GroupClass, GroupClassDto>` and implementing `ISingleResultSpecification`. Preserved the original query logic while removing redundant code from `SearchGroupClassRequest.cs`. Also, updated `using` directives in `SearchGroupClassRequest.cs` to reflect these changes.

--- src/Core/Application/Class/GroupClasses/SearchGroupClassRequest.cs
+++ src/Core/Application/Class/GroupClasses/SearchGroupClassRequest.cs
@@ -1,6 +1,6 @@
 using FSH.WebApi.Application.Class.GroupClasses.Dto;
+using FSH.WebApi.Application.Class.GroupClasses.Spec;
 using FSH.WebApi.Domain.Class;
-using Mapster;
 
 namespace FSH.WebApi.Application.Class.GroupClasses;
 
@@ -14,53 +14,6 @@ public enum ClassroomQueryType
     All = 0,
     MyClass = 1,
     SharedClass = 2
-}
-
-public class GroupClassBySearchRequestSpec : EntitiesByPaginationFilterSpec<GroupClass, GroupClassDto>
-{
-    public GroupClassBySearchRequestSpec(SearchGroupClassRequest request, DefaultIdType userId)
-        : base(request)
-    {
-        Query.Include(gc => gc.Classes)
-                .ThenInclude(c => c.TeacherPermissionInClasses)
-                    .ThenInclude(tp => tp.TeacherTeam)
-            .Include(gc => gc.Classes)
-                .ThenInclude(c => c.GroupPermissionInClasses)
-                    .ThenInclude(gp => gp.GroupTeacher)
-                        .ThenInclude(gt => gt.TeacherInGroups)
-                            .ThenInclude(tg => tg.TeacherTeam)
-            .Include(gc => gc.Classes)
-                .ThenInclude(c => c.UserClasses)
-                    .ThenInclude(uc => uc.Student)
-            .Where(gc => gc.CreatedBy == userId
-                        || gc.Classes.Any(c => c.TeacherPermissionInClasses.Any(tp => tp.TeacherTeam.TeacherId == userId)
-                        || c.GroupPermissionInClasses.Any(gp => gp.GroupTeacher.TeacherInGroups.Any(tig => tig.TeacherTeam.TeacherId == userId))
-                        || c.UserClasses.Any(uc => uc.Student.StId == userId)));
-
-        if (request.QueryType == ClassroomQueryType.MyClass)
-        {
-            Query.Where(gc => gc.CreatedBy == userId);
-        }
-        else if (request.QueryType == ClassroomQueryType.SharedClass)
-        {
-            Query.Where(gc => gc.Classes
-                .Any(c => c.TeacherPermissionInClasses.Any(tp => tp.TeacherTeam.TeacherId == userId)
-                        || c.GroupPermissionInClasses.Any(gp => gp.GroupTeacher.TeacherInGroups
-                                                    .Any(tig => tig.TeacherTeam.TeacherId == userId))));
-        }
-
-        Query.Select(gc => new GroupClassDto
-        {
-            Id = gc.Id,
-            Name = gc.Name,
-            Classes = gc.Classes
-            .Where(c => c.CreatedBy == userId
-                        || c.TeacherPermissionInClasses.Any(tp => tp.TeacherTeam.TeacherId == userId)
-                        || c.GroupPermissionInClasses.Any(gp => gp.GroupTeacher.TeacherInGroups.Any(tig => tig.TeacherTeam.TeacherId == userId))
-                        || c.UserClasses.Any(uc => uc.Student.StId == userId)).ToList().Adapt<List<ClassViewListDto>>(),
-            CreatedBy = gc.CreatedBy
-        });
-    }
 }
 
 public class SearchGroupClassesRequestHandler : IRequestHandler<SearchGroupClassRequest, PaginationResponse<GroupClassDto>>
