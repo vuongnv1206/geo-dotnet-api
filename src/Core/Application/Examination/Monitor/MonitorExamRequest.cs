99936608e2e3bdc65ac5043fda65c2ad5333b024
nguyenvancaokyfpt <kynvche163260@fpt.edu.vn>
2024-08-06 10:02:50 +0700

    feat: Add MonitorExamRequest and MonitorExamRequestHandler classes
    The code changes introduce a new file `MonitorExamRequest.cs` which contains the `MonitorExamRequest` class and its corresponding handler `MonitorExamRequestHandler`. These classes are used for monitoring exams and handling the request to monitor an exam.

---
+++ src/Core/Application/Examination/Monitor/MonitorExamRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace FSH.WebApi.Application.Examination.Monitor;
+
+public class MonitorExamRequest : IRequest<DefaultIdType>
+{
+    public DefaultIdType PaperId { get; set; }
+}
+
+public class MonitorExamRequestHandler : IRequestHandler<MonitorExamRequest, DefaultIdType>
+{
+    public Task<DefaultIdType> Handle(MonitorExamRequest request, CancellationToken cancellationToken)
+    {
+        // not implemented
+        return Task.FromResult(new DefaultIdType());
+    }
+}
