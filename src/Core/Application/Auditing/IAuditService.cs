1cb8f0120a31ca2a2c9eea96b70e7b5c1aebea9f
nguyenvancaokyfpt <kynvche163260@fpt.edu.vn>
2024-08-19 00:18:50 +0700

    fix: Filter out "Clone" tables in AuditService

--- src/Core/Application/Auditing/IAuditService.cs
+++ src/Core/Application/Auditing/IAuditService.cs
@@ -3,5 +3,5 @@ namespace FSH.WebApi.Application.Auditing;
 public interface IAuditService : ITransientService
 {
     Task<PaginationResponse<AuditDto>> GetUserTrailsAsync(GetMyAuditLogsRequest request);
-    Task<List<string>> GetResourceName();
+    Task<List<string?>> GetResourceName();
 }
