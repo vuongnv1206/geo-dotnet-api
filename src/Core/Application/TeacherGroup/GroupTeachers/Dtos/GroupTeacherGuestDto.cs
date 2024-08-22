45964937d6018f10c1772e4e7946ba3fcf64f87f
ducNT <cavoixanh2112@gmail.com>
2024-08-22 16:41:29 +0700

    get detail group for guest

---
+++ src/Core/Application/TeacherGroup/GroupTeachers/Dtos/GroupTeacherGuestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace FSH.WebApi.Application.TeacherGroup.GroupTeachers;
+public class GroupTeacherGuestDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public string? QrCode { get; set; }
+    public string? JoinLink { get; set; }
+    public string? AdminGroup { get; set; }
+    public Guid CreatedBy { get; set; }
+}
